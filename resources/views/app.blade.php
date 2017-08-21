7d4e88ee88f30e4331997dca84662d2ffb2204cd
Robert Wright <robertwrightv@gmail.com>
2017-08-21 12:30:39 +0100

    fixes url paths

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -34,7 +34,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="{{ env('APP_FOLDER') }}/"><span class="brand-icon-primary glyphicon glyphicon-earphone" aria-hidden="true"></span> {{ config('app.name') }}</a>
+          <a class="navbar-brand" href="/{{ env('APP_FOLDER') }}"><span class="brand-icon-primary glyphicon glyphicon-earphone" aria-hidden="true"></span> {{ config('app.name') }}</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
@@ -78,14 +78,14 @@
               <a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->username }} <span class="caret"></span></a>
               <ul class="dropdown-menu">
                 <li><a href="https://mystats.services.conduent.com/support"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span>Help</a></li>
-                <li><a href="{{ env('APP_FOLDER') }}/logout"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span> Logout</a></li>
+                <li><a href="/{{ env('APP_FOLDER') }}/logout"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span> Logout</a></li>
               </ul>
             </li>
           </ul>
         </div>
         @else
         <div class="navbar-header">
-          <a class="navbar-brand" href="{{ env('APP_FOLDER') }}/"><span class="brand-icon-primary glyphicon glyphicon-earphone" aria-hidden="true"></span> {{ config('app.name') }}</a>
+          <a class="navbar-brand" href="/{{ env('APP_FOLDER') }}/"><span class="brand-icon-primary glyphicon glyphicon-earphone" aria-hidden="true"></span> {{ config('app.name') }}</a>
         </div>
         @endif
 
