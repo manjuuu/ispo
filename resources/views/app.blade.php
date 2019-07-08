f4b7bf0f41aefe870f1192512de0af8a2b9aa32d
D N <C5099941@acsind.com>
2019-07-08 19:40:21 +0530

    new requirement

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -55,8 +55,7 @@
               <li><a href="{{ action('QueueController@index') }}">Queues</a></li>
               <li><a href="{{ action('ResponseController@forms') }}">Reports</a></li>
               <li><a href="/list_all_disposes">All disposes</a></li>
-
-               <!-- <li><a href="/logs">Serializes data</a></li> -->
+              <!-- <li><a href="/logs">Serializes data</a></li> -->
           </ul>
           <ul class="nav navbar-nav navbar-right">
             <li class="dropdown">
